ad8a4a72979aa3f71518d45564096a8da421155d
Nelson Chen <nelsonchen5@gmail.com>
2018-03-09 12:34:53 -0800

    test successfail

--- socket-server/src/clientEvents.js
+++ socket-server/src/clientEvents.js
@@ -49,17 +49,23 @@ const clientRun = async ({ io, room }, payload) => {
   console.log('this is the client run payload:', payload)
 
   try {
-    const { data } = await axios.post(`${url}/submit-code`, { code: text, test: test});
-    const stdout = data;
-    const testData= await axios.post(`${url}/submit-test`, {test: test});
-    const testout = JSON.parse(testData.config.data).test
+    const { data } = await axios.post(`${url}/submit-code`, { code: text});
+    let stdout = data;
+    const testdata= await axios.post(`${url}/submit-test`, {test: test});
+    const testout = testdata.data
     
     console.log('this is stdout', stdout)
-    console.log('this is testoutput',testout)
+    console.log('this is testout',testout)
     
     if(stdout === testout){
-    serverRun({ io, room }, { stdout, email });
+
+      stdout = 'success'
+    } else{
+      stdout = 'fail'
+      
     }
+    serverRun({ io, room }, { stdout, email });
+    
 
   } catch (e) {
     success('error posting to coderunner service from socket server. e = ', e);
