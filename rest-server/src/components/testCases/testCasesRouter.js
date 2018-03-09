45cb9e0a03042231c4c21b1206ab082c08ee27bb
Michael Wachell <wachell@gmail.com>
2018-03-08 17:13:15 -0800

    added get test by challenge id because reasons

--- rest-server/src/components/testCases/testCasesRouter.js
+++ rest-server/src/components/testCases/testCasesRouter.js
@@ -1,12 +1,17 @@
 import express from 'express';
 
 import {
-  addTestCaseController
+  addTestCaseController,
+  testGetter
 } from './testCasesControllers';
 
 const router = express.Router();
 
 router.route('/')
   .post(addTestCaseController);
+
+router.route('/:challenge_id')
+  .get(testGetter);
+
 
 export default router;
